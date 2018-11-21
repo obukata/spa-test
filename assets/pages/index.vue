90f0f3fd35969c84495e2871ff6374c56be92ae4
生方 健成 <obukata@mj-inc.jp>
2018-11-21 10:17:18 +0900

    いい感じのスタイルにしてやる。

--- assets/pages/index.vue
+++ assets/pages/index.vue
@@ -9,14 +9,29 @@
 			earum reiciendis dolor maxime dolore quam tenetur.
 		</p>
 		<article class="article">
-			<section class="section contents-section">
-				<div class="contents-body section-body">
-					<h2 class="contents-title section-title style--font-en style--font-black">Contents</h2>
-					<ul class="contents-list style--column style--font-en style--font-bold">
-						<li class="contents-item"><router-link to="/">top</router-link></li>
-						<li class="contents-item"><router-link to="/about">about</router-link></li>
-						<li class="contents-item"><router-link to="/contact">contact</router-link></li>
-					</ul>
+			<section class="section panel-section">
+				<div class="style--card-column">
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card01.jpg" alt=""></router-link>
+					</div>
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card02.jpg" alt=""></router-link>
+					</div>
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card03.jpg" alt=""></router-link>
+					</div>
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card04.jpg" alt=""></router-link>
+					</div>
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card05.jpg" alt=""></router-link>
+					</div>
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card06.jpg" alt=""></router-link>
+					</div>
+					<div class="panel-card">
+						<router-link to="/"><img src="/assets/images/common/img_card07.jpg" alt=""></router-link>
+					</div>
 				</div>
 			</section>
 		</article>
